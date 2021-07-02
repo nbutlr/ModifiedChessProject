2825654d7eb472970481d16256dd1ad8814cf2e9
nbutlr <05257@farnborough.ac.uk>
2021-07-02 12:13:38 +0100

    More stuff with CSS and some html / js changes

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-class Pawn {
+/* This is my main javascript file where the Modified Chess game will be handled and run
+Created by Nathan Butler starting 14/06/21 for my A-level Computer Science project */
+
+class Pawn { 
     constructor(square, moved, colour) {
         this.square = square;
         this.moved = moved;
@@ -11,40 +14,73 @@ class Pawn {
         }
     }
 
-    get getSquare() {
+    getSquare() {
         return this.square;
     }
     
-    get hasMoved() {
+    hasMoved() {
         return this.moved;
     }
 
-    set destroy() {
+    // move(square) {
+
+    // }
+
+    hasMoved(bool) {
+        this.moved = bool;
+    }
+
+    destroy() {
         this.onBoard = false;
     }
+}
+
+class Knight {
+    constructor(square, colour) {
+        this.square = square;
+        this.colour = colour;
+        this.onBoard = true;
+        if (colour == "white") {
+            document.getElementById(square).innerHTML = "&#9816;";
+        } else {
+            document.getElementById(square).innerHTML = "&#9822;";
+        }
+    }
+
+    getSquare() {
+        return this.square;
+    }
+
+    destroy() {
+        this.onBoard = false;
+    }
+}
+
+function convertSquare(square) {
+    return (square.charCodeAt(0)-64, parseInt(square.slice(1)));
 }
 
 function initialiseBoard() {
     document.getElementById("board").innerHTML = "";
     for (i=8;i>0;i--) {
-        document.getElementById("board").innerHTML += "<p>";
+        document.getElementById("board").innerHTML += "<div>";
         for (j=1;j<9;j++) {
             file = String.fromCharCode(j+64);
             document.getElementById("board").innerHTML += "<span id='"+file+i.toString()+"' class='"+file+" "+i.toString()+"'>"+file+i.toString()+"</span>";
         }
-        document.getElementById("board").innerHTML += "</p>";
+        document.getElementById("board").innerHTML += "</div>";
     }
 }
-initialiseBoard();
-pawns = [];
-for (i=1;i<9;i++) {
-    pawns.push(+new Pawn(String.fromCharCode(i+64)+"2", false, "white"));
-}
-for (j=1;j<9;j++) {
-    pawns.push(+new Pawn(String.fromCharCode(j+64)+"7", false, "black"));
+
+function initialisePawns() {
+    for (i=1;i<9;i++) {
+        pawns.push(+new Pawn(String.fromCharCode(i+64)+"2", false, "white"));
+    }
+    for (j=1;j<9;j++) {
+        pawns.push(+new Pawn(String.fromCharCode(j+64)+"7", false, "black"));
+    }
 }
 
-// function convertSquare(square) {
-//     console.log(square.charCodeAt(0)-64, parseInt(square.slice(1)));
-// }
-// convertSquare(pawn1.getSquare);
+initialiseBoard();
+pawns = [];
+initialisePawns();
